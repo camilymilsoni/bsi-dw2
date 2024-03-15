48c694af694898e9a3a5be31213e4b746a7dd3ce
Camily Milsoni <camily_milsoni@hotmail.com>
2024-03-15 19:12:51 -0300

    Adicionando Aula 06

---
+++ Aula06/Praticando1/faq.php
@@ -0,0 +1,32 @@
+<?php
+require 'header.php';
+?>
+
+<div class="bg-light p-4 mb-4 rounded">
+    <h1 class="text-center">Página FAQs</h1>
+</div>
+<div class="row">
+    <div class="col-md-6 text-center">
+        <img class="img-thumbnail m-1 rounded" width="86%" src="https://vda.adv.br/wp-content/uploads/2021/07/Lorem-Ipsum-alternatives-768x492-1-300x192.png" alt="Lorem Ipsum">
+    </div>
+    <div class="col-md-6">
+        <ul>
+            <li>
+                <h5>FAQ 1</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sit amet egestas sapien. Fusce porttitor tellus vel eleifend dictum. Aenean sed neque sit amet risus eleifend pulvinar.</p>
+            </li>
+            <li>
+                <h5>FAQ 2</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sit amet egestas sapien. Fusce porttitor tellus vel eleifend dictum. Aenean sed neque sit amet risus eleifend pulvinar.</p>
+            </li>
+            <li>
+                <h5>FAQ 3</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sit amet egestas sapien. Fusce porttitor tellus vel eleifend dictum. Aenean sed neque sit amet risus eleifend pulvinar.</p>
+            </li>
+        </ul>
+    </div>
+</div>
+
+<?php
+require 'footer.php';
+?>
